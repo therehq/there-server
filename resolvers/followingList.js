2a94e1874814cd8ad2238c5cfc1ec36cc2ad9f92
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-17 17:18:55 +0330

    Update following list

--- resolvers/followingList.js
+++ resolvers/followingList.js
@@ -1,7 +1,11 @@
 import { flag } from 'country-emoji'
 
 export default async (obj, args, ctx) => {
-  const [wrappedFollowings, wrappedPeople, wrappedPlaces] = await Promise.all([
+  const [
+    wrappedFollowings,
+    wrappedManualPeople,
+    wrappedManualPlaces,
+  ] = await Promise.all([
     ctx.user.getFollowing(),
     ctx.user.getManualPeople(),
     ctx.user.getManualPlaces(),
@@ -9,33 +13,36 @@ export default async (obj, args, ctx) => {
 
   // These are not simple objects, but Sequilize special objects
   // We need to extract pure user data from them
-  return [
+  const people = [
     ...wrappedFollowings.map(wrapped => {
       const following = wrapped.get({ plain: true })
       // Privacy!
       delete following.email
       delete following.twitterId
-
       following.__resolveType = 'User'
       return following
     }),
-    ...wrappedPeople.map(wrapped => {
+    ...wrappedManualPeople.map(wrapped => {
       const person = wrapped.get({ plain: true })
       person.__resolveType = 'ManualPerson'
       return person
     }),
-    ...wrappedPlaces.map(wrapped => {
-      const place = wrapped.get({ plain: true })
-
-      if (!place.photoUrl) {
-        // Add the flag if it has no photo
-        const locationParts = place.fullLocation.split(',')
-        const countryName = locationParts[locationParts.length - 1]
-        place.countryFlag = flag(countryName)
-      }
-
-      place.__resolveType = 'ManualPlace'
-      return place
-    }),
   ]
+
+  const places = wrappedManualPlaces.map(wrapped => {
+    const place = wrapped.get({ plain: true })
+
+    if (!place.photoUrl) {
+      // Add the flag if it has no photo
+      const locationParts = place.fullLocation.split(',')
+      const countryName = locationParts[locationParts.length - 1]
+      place.countryFlag = flag(countryName)
+    }
+
+    place.__resolveType = 'ManualPlace'
+    return place
+  })
+
+  const followings = { places, people }
+  return followings
 }
