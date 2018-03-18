42f097b2db06bf72856b4108038e7848b6c57e7c
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-18 17:02:19 +0330

    Fetch only what is requested in followingList

--- resolvers/followingList.js
+++ resolvers/followingList.js
@@ -1,19 +1,49 @@
+import getFieldNames from 'graphql-list-fields'
 import { flag } from 'country-emoji'
 
-export default async (obj, args, ctx) => {
-  const [
-    wrappedFollowings,
-    wrappedManualPeople,
-    wrappedManualPlaces,
-  ] = await Promise.all([
-    ctx.user.getFollowing({ order: [['createdAt', 'ASC']] }),
-    ctx.user.getManualPeople({ order: [['createdAt', 'ASC']] }),
-    ctx.user.getManualPlaces({ order: [['createdAt', 'ASC']] }),
-  ])
-  //{ order: [['createdAt', 'ASC']] }
-  // These are not simple objects, but Sequilize special objects
-  // We need to extract pure user data from them
-  const people = [
+export default async (obj, args, ctx, info) => {
+  // Check what fields are requested
+  const fields = getFieldNames(info)
+  const needsPeople = Boolean(fields.find(f => f.startsWith(`people`)))
+  const needsPlaces = Boolean(fields.find(f => f.startsWith(`places`)))
+
+  // Prepare queries we might execute
+  // (we need to wrap them in functions or
+  // they'll fire right away!)
+  const followingUsers = () =>
+    ctx.user.getFollowing({
+      order: [['createdAt', 'ASC']],
+    })
+  const manualPeople = () =>
+    ctx.user.getManualPeople({
+      order: [['createdAt', 'ASC']],
+    })
+  const manualPlaces = () =>
+    ctx.user.getManualPlaces({
+      order: [['createdAt', 'ASC']],
+    })
+
+  let wrappedFollowings, wrappedManualPeople, wrappedManualPlaces
+  // Fetch only the required data by the user
+  if (needsPeople && needsPlaces) {
+    // Fetch both
+    ;[
+      wrappedFollowings,
+      wrappedManualPeople,
+      wrappedManualPlaces,
+    ] = await Promise.all([followingUsers(), manualPeople(), manualPlaces()])
+  } else if (needsPeople && !needsPlaces) {
+    // Fetch only people
+    ;[wrappedFollowings, wrappedManualPeople] = await Promise.all([
+      followingUsers(),
+      manualPeople(),
+    ])
+  } else if (needsPlaces && !needsPeople) {
+    // Fetch only places
+    wrappedManualPlaces = await manualPlaces()
+  }
+
+  const people = needsPeople && [
     ...wrappedFollowings.map(wrapped => {
       const following = wrapped.get({ plain: true })
       // Privacy!
@@ -29,19 +59,21 @@ export default async (obj, args, ctx) => {
     }),
   ]
 
-  const places = wrappedManualPlaces.map(wrapped => {
-    const place = wrapped.get({ plain: true })
+  const places =
+    needsPlaces &&
+    wrappedManualPlaces.map(wrapped => {
+      const place = wrapped.get({ plain: true })
 
-    if (!place.photoUrl && place.fullLocation) {
-      // Add the flag if it has no phFolloto
-      const locationParts = place.fullLocation.split(',')
-      const countryName = locationParts[locationParts.length - 1]
-      place.countryFlag = flag(countryName)
-    }
+      if (!place.photoUrl && place.fullLocation) {
+        // Add the flag if it has no phFolloto
+        const locationParts = place.fullLocation.split(',')
+        const countryName = locationParts[locationParts.length - 1]
+        place.countryFlag = flag(countryName)
+      }
 
-    place.__resolveType = 'ManualPlace'
-    return place
-  })
+      place.__resolveType = 'ManualPlace'
+      return place
+    })
 
   const followings = { places, people }
   return followings
