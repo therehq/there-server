874ac94827f65bc1831b90feb5c910e8b721ea8b
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-04-17 18:38:02 +0430

    Send authenticated request to Github, Cache Github results

--- helpers/github.js
+++ helpers/github.js
@@ -1,15 +1,39 @@
 import fetch from 'node-fetch'
+import Lru from 'lru-cache'
+import ms from 'ms'
+
+const cache = Lru({
+  max: 10,
+  maxAge: ms('12h'),
+})
+
+const { GH_TOKEN_RELEASES } = process.env
+const latestReleaseAssetsKey = 'github-latest-release'
 
 export const getLatestReleaseDlLink = async () => {
-  const response = await fetch(
-    'https://api.github.com/repos/therepm/there-desktop/releases/latest',
-  )
-  const data = await response.json()
+  const cachedAssets = cache.get(latestReleaseAssetsKey)
 
-  if (!data.assets) {
-    throw `Sorry, couldn't find the file now.`
+  let assets
+  if (!cachedAssets) {
+    // We should fetch for the first time
+    console.log('requested....')
+    const response = await fetch(
+      'https://api.github.com/repos/therepm/there-desktop/releases/latest',
+      { headers: { Authorization: `token ${GH_TOKEN_RELEASES}` } },
+    )
+    ;({ assets } = await response.json())
+    cache.set(latestReleaseAssetsKey, assets)
+  } else {
+    // The assets have been cached
+    console.log('used cache...')
+    assets = cachedAssets
   }
 
-  const asset = data.assets.find(asset => asset.name.endsWith('-mac.zip'))
+  // Check if there is no assets, return an error
+  if (!assets) {
+    throw new Error(`Sorry, couldn't find the file now.`)
+  }
+
+  const asset = assets.find(asset => asset.name.endsWith('-mac.zip'))
   return asset.browser_download_url
 }
