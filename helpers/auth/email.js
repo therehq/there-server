7398f8dc978adce5acc7845870dbcecd0754bd86
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-05-25 13:22:20 +0430

    Add new sign in email method

---
+++ helpers/auth/email.js
@@ -0,0 +1,113 @@
+import {
+  encode as jwtSimpleEncode,
+  decode as jwtSimpleDecode,
+} from 'jwt-simple'
+import v4 from 'uuid/v4'
+
+// Utilities
+import { uploadToStorageFromUrl } from '../google/uploadToStorage'
+import { getGravatarUrl } from '../gravatar'
+import { mixpanel } from '../mixpanel'
+
+// Models
+import { User } from '../../models'
+
+const { JWT_SECRET } = process.env
+
+export const uploadGravatarToStorage = async (userId, email) => {
+  let gravatar = getGravatarUrl(email, { size: 84, default: '404' })
+  let photoCloudObject = null
+  let photoUrl = null
+
+  // Only try to upload photo if there is one
+  if (gravatar) {
+    // Upload photo to our storage
+    try {
+      // Set url and cloud object directly from the upload function
+      ;({
+        publicUrl: photoUrl,
+        cloudObject: photoCloudObject,
+      } = await uploadToStorageFromUrl(userId, gravatar))
+    } catch (err) {
+      Raven.captureException(err)
+      console.error(err)
+    }
+  }
+
+  return { photoUrl, photoCloudObject }
+}
+
+export const signInUserByEmail = io => async (req, res, next) => {
+  const { token, socketId } = req.query
+
+  if (!token) {
+    res.send('Something went wrong. Please copy the link carefully again.')
+    return
+  }
+
+  let email
+  try {
+    ;({ email } = jwtSimpleDecode(token, JWT_SECRET))
+  } catch (err) {
+    console.log(err)
+    res.send('Incomplete code, please copy the link carefully again.')
+    return
+  }
+
+  try {
+    // Find the client to notify of result
+    const socket = io.to(socketId)
+
+    // Fetch user data if user is already saved in the DB
+    const signedUpUser = await User.findOne({ where: { email } })
+
+    // If user has already signed up, it's done
+    if (signedUpUser) {
+      req.user = signedUpUser.get({ plain: true })
+      next()
+      return
+    }
+
+    // Notify the user that we're registering them
+    socket.emit('signingup')
+
+    // Get user data or create a new user
+    const userId = v4()
+
+    // Upload the photo to our storage
+    const { photoUrl, photoCloudObject } = await uploadGravatarToStorage(
+      userId,
+      email,
+    )
+
+    // Create user
+    const user = await User.create({
+      id: userId,
+      email,
+      photoUrl,
+      photoCloudObject,
+    })
+
+    // Follow himself/herself
+    await user.addFollowing(userId)
+
+    // Track sign up
+    try {
+      mixpanel.track('Sign Up', {
+        distinct_id: userId,
+        method: 'email',
+        email,
+        userId,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+
+    // Pass user data
+    req.user = user.get({ plain: true })
+    next()
+  } catch (err) {
+    console.error(err)
+    return next(err)
+  }
+}
--- helpers/auth/passport.js
+++ helpers/auth/passport.js
@@ -4,6 +4,16 @@ import passport from 'passport'
 import { twitterStrategy } from './twitter'
 import { jwtStrategy, encodeJwt } from './jwt'
 import { serializeUser, deserializeUser } from './session'
+import { generateRandomPhrase } from '../../utils/randomPhrase'
+import {
+  encode as jwtSimpleEncode,
+  decode as jwtSimpleDecode,
+} from 'jwt-simple'
+import config from '../../utils/config'
+import { User } from '../../models'
+import { uploadGravatarToStorage, signInUserByEmail } from './email'
+
+const { JWT_SECRET } = process.env
 
 /**
  * Setup authentication with PassportJS
@@ -21,11 +31,11 @@ export const setupPassportAuth = (app, io) => {
   app.use(passport.initialize())
   app.use(passport.session())
 
+  ///////// TWITTER ///////////
   app.get(
     '/auth/twitter',
     // Save socketId in the session
     (req, res, next) => {
-      req.query.socketId
       req.session.socketId = req.query.socketId
       next()
     },
@@ -57,6 +67,59 @@ export const setupPassportAuth = (app, io) => {
     },
   )
 
+  ///////// EMAIL ///////////
+  app.post('/auth/email', (req, res) => {
+    const { email, socketId } = req.body
+
+    // Check for socketId to save for use in callback later
+    if (!socketId) {
+      res.json({
+        sent: false,
+        message: 'No socket. Live support at there.pm',
+      })
+      return
+    }
+
+    if (email) {
+      const securityCode = generateRandomPhrase()
+      const token = jwtSimpleEncode({ email: email.toLowerCase() }, JWT_SECRET)
+      const callbackUrl = `${
+        config.apiUrl
+      }/auth/email/callback?socketId=${socketId}&token=${token}`
+
+      // TODO: Send email
+      console.log(
+        `Log on by email, securityCode: ${securityCode}, callbackUrl: ${callbackUrl}`,
+      )
+
+      res.json({ sent: true, securityCode })
+    } else {
+      res.json({ sent: false, message: 'No email provided.' })
+    }
+  })
+  app.get('/auth/email/callback', signInUserByEmail(io), async (req, res) => {
+    // Get client's socketId from session
+    const socket = io.to(req.query.socketId)
+    // Check if sign in failed
+    if (!req.user && !req.user.id) {
+      socket.emit('signin-failed')
+      // Wait to finish socket
+      await sleep(200)
+      res.send(`<script>window.close();</script>`)
+      return
+    }
+    // Then it was successful...
+    // Generate JWT token
+    const jwtToken = encodeJwt(req.user.id)
+    // Send token to client
+    socket.emit('signin-succeeded', { jwtToken, user: req.user })
+    // Wait to finish socket
+    await sleep(200)
+    // And close the window
+    res.send(`<script>window.close();</script>`)
+  })
+
+  ///////// JWT ///////////
   app.get('/auth/jwt', passport.authenticate('jwt'), (req, res) => {
     res.send(`userId: ${req.userId}`)
   })
