6ac443bcd23d1c2a019f9d6dccaf736517b85e3d
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-05-27 14:54:44 +0430

    Add dev mixpanel project

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,10 @@
 const { NOW } = process.env
 const host = NOW ? 'https://api.there.pm' : 'http://localhost:9900'
+const mixpanelProjectToken = NOW
+  ? 'e7859c5640d175b8f34d425735fba85e' // PROD
+  : '31a53a5d9fb1a091846b5abffac684e7' // DEV
 
 export default {
   apiUrl: host,
-  mixpanelProjectToken: `e7859c5640d175b8f34d425735fba85e`,
+  mixpanelProjectToken,
 }
