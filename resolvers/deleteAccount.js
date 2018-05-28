04f604b61e5629139c698e22a889306e642cd311
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-05-28 22:51:57 +0430

    Track delete account in Mixpanel

--- resolvers/deleteAccount.js
+++ resolvers/deleteAccount.js
@@ -1,4 +1,17 @@
+import { mixpanel } from '../helpers/mixpanel'
+
 export default async (obj, args, ctx, info) => {
+  const userId = ctx.userId
+
+  try {
+    mixpanel.track('Delete Account', {
+      distinct_id: userId,
+      userId,
+    })
+  } catch (err) {
+    console.error(err)
+  }
+
   // Delete user and its data
   await ctx.user.destroy({ force: true })
   return true
